01f6818c3218da8b8a42d528165f15d8d2970b2f
llg <1348930321@qq.com>
2018-06-27 21:57:05 +0800

    add button

--- game1/alien_invasion.py
+++ game1/alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from game_stats import GameStats
+from button import Button
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -13,6 +14,8 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    play_button = Button(ai_settings, screen, "Play")
 
     ship = Ship(ai_settings, screen)
 
@@ -32,7 +35,7 @@ def run_game():
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
             
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 
 run_game()
